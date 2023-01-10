32700767cdc1976fd3eb1703410b415254e98bdd
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-01-11 00:59:17 +0100

    1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/React-example/"> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
